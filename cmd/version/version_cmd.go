f25df452009eb5ab765ea1b5149841ac1f2e4d6f
Felipe Duzzi <duzzifelipe@gmail.com>
2025-07-22 17:30:21 -0300

    chore: dinamically get version from build

--- cmd/version/version_cmd.go
+++ cmd/version/version_cmd.go
@@ -6,13 +6,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var version string
+
 func getVersionCommand() *cobra.Command {
 	return &cobra.Command{
 		Use:   "version",
 		Short: "Print the version number of UpDuck",
 		Long:  `Show version information for UpDuck CLI.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("UpDuck v1.0.0")
+			fmt.Printf("UpDuck %s\n", version)
 		},
 	}
 }
